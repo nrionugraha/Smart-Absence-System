1c7d917c8d5861ec65a345c337a77000bc5bff00
Nathanael Rio Nugraha <92268886+nrionugraha@users.noreply.github.com>
2025-03-04 20:18:34 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-const privateKey = "ddf8b628eec1be234a083c2fe97d303961bf291f5d194f86b4e79836b2d42a94";
+const privateKey = "PRIVATE_KEY";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
   hardhat: {
     },
   sepolia: {
-    url: "https://sepolia.infura.io/v3/cdb76b86ed444a33891442a4ebb8ad7b",
+    url: "https://sepolia.infura.io/v3/<key>",
     accounts: [privateKey]
     }
   }
